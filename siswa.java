5b58f97e5299d7218514ae91e20948acdf136520
florensialaia <163755415+florensialaia@users.noreply.github.com>
2024-03-18 21:23:35 +0700

    Add files via upload

---
+++ siswa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PeogramanObjek;
+    class Pelajar{
+        String Nim;
+        String Agama ;
+        String Kendaraan;
+    }
+/**
+ *
+ * @author INFINIX
+ */
+public class siswa {
+    Orang belajar1 = new Pelajar();
+    Orang belajar2 = new Pelajar();
+    Orang belajar3 = new Pelajar();
+    
+    belajar1.Nim = "22100038";
+    belajar1.Agama = "3";
+    belajar1.Kendaraan= "perempuan";
+    
+    System.out.println("nomor siswa     : " + orang.Nim);
+    System.out.println("kelas           : " + orang.Kelas);
+    System.out.println("jenis kelamin   : " + orang.Jeniskelamin);
+}
